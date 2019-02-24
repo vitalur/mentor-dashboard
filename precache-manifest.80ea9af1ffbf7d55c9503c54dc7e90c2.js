6d8493160854bcaa4850f8395c98c661bc0bb0ed
Vitalik Tyrlya <39513998+vitalur@users.noreply.github.com>
2019-02-24 23:20:14 +0300

    feat: create mentor dashboard

---
+++ precache-manifest.80ea9af1ffbf7d55c9503c54dc7e90c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3cb9980b32264e5ccc4",
+    "url": "/mentor-dashboard/static/css/main.33f29f41.chunk.css"
+  },
+  {
+    "revision": "a3cb9980b32264e5ccc4",
+    "url": "/mentor-dashboard/static/js/main.a3cb9980.chunk.js"
+  },
+  {
+    "revision": "5357f7305c488abea30c",
+    "url": "/mentor-dashboard/static/js/runtime~main.5357f730.js"
+  },
+  {
+    "revision": "00ec0959a47ed983999e",
+    "url": "/mentor-dashboard/static/js/2.00ec0959.chunk.js"
+  },
+  {
+    "revision": "34bbae1199e4a684533492031bae948a",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
